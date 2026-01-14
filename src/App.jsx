fc30a639518a9dd5661acf535fa23829cacfd6ee
James Taylor <jamestay97@gmail.com>
2026-01-14 02:24:47 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,22 @@ import Layout from './Layout';
 import Home from './components/public/Home';
 import AdminPage from './components/admin/AdminPage';
 import AdminLogin from './components/admin/AdminLogin';
+import { Navigate } from 'react-router-dom';
 
 function App() {
   return (
-    <Router> 
+    <Router>
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<AdminLogin />} />
           <Route path="/admin" element={<AdminPage />} />
+          <Route path="/gallery" element={<Gallery />} />
+          
+          {/* ADD THIS LINE: If the route is weird (like an error), go back Home */}
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Layout>
     </Router>
   );
 }
-
-export default App;
